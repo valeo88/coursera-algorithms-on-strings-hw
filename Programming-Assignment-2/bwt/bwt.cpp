c44778f149520209ec96a130a9ce2fb1335beeed
Valentin Popov <valeo.popov@gmail.com>
2018-08-09 10:37:22 +0300

    bwt transform is implemented

--- Programming-Assignment-2/bwt/bwt.cpp
+++ Programming-Assignment-2/bwt/bwt.cpp
@@ -13,6 +13,17 @@ string BWT(const string& text) {
   string result = "";
 
   // write your code here
+  const size_t n = text.size();
+  vector<string> m;
+  for (size_t i = 0; i < n; ++i) {
+    string s = text.substr(i, n-i);
+    s += text.substr(0,i);
+    m.push_back(s);
+  }
+  std::sort(m.begin(),m.end());
+  for (const string& s : m) {
+    result += s[n-1];
+  }
 
   return result;
 }
