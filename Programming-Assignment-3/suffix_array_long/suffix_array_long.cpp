01abb81b1d15e5a9b9f80588a860313caac873ff
Valentin Popov <valeo.popov@gmail.com>
2018-08-23 12:23:43 +0300

    building suffix array is implemented

--- Programming-Assignment-3/suffix_array_long/suffix_array_long.cpp
+++ Programming-Assignment-3/suffix_array_long/suffix_array_long.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <map>
 
 using std::cin;
 using std::cout;
@@ -11,17 +12,97 @@ using std::make_pair;
 using std::pair;
 using std::string;
 using std::vector;
+using std::map;
 
-// Build suffix array of the string text and
-// return a vector result of the same length as the text
-// such that the value result[i] is the index (0-based)
-// in text where the i-th lexicographically smallest
-// suffix of text starts.
-vector<int> BuildSuffixArray(const string& text) {
-  vector<int> result;
-  // Implement this function yourself
-  return result;
+// Structure to store information of a suffix
+struct suffix
+{
+    int index; // To store original index
+    int rank[2]; // To store ranks and next rank pair
+};
+
+// A comparison function used by sort() to compare two suffixes
+// Compares two pairs, returns 1 if first pair is smaller
+int cmp(struct suffix a, struct suffix b)
+{
+    return (a.rank[0] == b.rank[0])? (a.rank[1] < b.rank[1] ?1: 0):
+               (a.rank[0] < b.rank[0] ?1: 0);
 }
+
+vector<int> BuildSuffixArray(const string& txt)
+{
+    const size_t n = txt.size();
+    // A structure to store suffixes and their indexes
+    struct suffix suffixes[n];
+
+    // Store suffixes and their indexes in an array of structures.
+    // The structure is needed to sort the suffixes alphabatically
+    // and maintain their old indexes while sorting
+    for (int i = 0; i < n; i++)
+    {
+        suffixes[i].index = i;
+        suffixes[i].rank[0] = txt[i] - 'a';
+        suffixes[i].rank[1] = ((i+1) < n)? (txt[i + 1] - 'a'): -1;
+    }
+
+    // Sort the suffixes using the comparison function
+    // defined above.
+    std::sort(suffixes, suffixes+n, cmp);
+
+    // At this point, all suffixes are sorted according to first
+    // 2 characters.  Let us sort suffixes according to first 4
+    // characters, then first 8 and so on
+    int ind[n];  // This array is needed to get the index in suffixes[]
+                 // from original index.  This mapping is needed to get
+                 // next suffix.
+    for (int k = 4; k < 2*n; k = k*2)
+    {
+        // Assigning rank and index values to first suffix
+        int rank = 0;
+        int prev_rank = suffixes[0].rank[0];
+        suffixes[0].rank[0] = rank;
+        ind[suffixes[0].index] = 0;
+
+        // Assigning rank to suffixes
+        for (int i = 1; i < n; i++)
+        {
+            // If first rank and next ranks are same as that of previous
+            // suffix in array, assign the same new rank to this suffix
+            if (suffixes[i].rank[0] == prev_rank &&
+                    suffixes[i].rank[1] == suffixes[i-1].rank[1])
+            {
+                prev_rank = suffixes[i].rank[0];
+                suffixes[i].rank[0] = rank;
+            }
+            else // Otherwise increment rank and assign
+            {
+                prev_rank = suffixes[i].rank[0];
+                suffixes[i].rank[0] = ++rank;
+            }
+            ind[suffixes[i].index] = i;
+        }
+
+        // Assign next rank to every suffix
+        for (int i = 0; i < n; i++)
+        {
+            int nextindex = suffixes[i].index + k/2;
+            suffixes[i].rank[1] = (nextindex < n)?
+                                  suffixes[ind[nextindex]].rank[0]: -1;
+        }
+
+        // Sort the suffixes according to first k characters
+        std::sort(suffixes, suffixes+n, cmp);
+    }
+
+    // Store indexes of all sorted suffixes in the suffix array
+    vector<int> suffixArr(n,0);
+    for (int i = 0; i < n; i++)
+        suffixArr[i] = suffixes[i].index;
+
+    // Return the suffix array
+    return  suffixArr;
+}
+
 
 int main() {
   string text;
